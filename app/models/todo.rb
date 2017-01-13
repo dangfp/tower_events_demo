78fc0f657b6ea708a3801bb95a155a6aba5938f6
dfp <dangfeipeng@gmail.com>
2017-01-13 10:40:21 +0800

    增加任务状态机

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,10 +1,38 @@
 class Todo < ApplicationRecord
-  include Resourceable
+  include AASM
   include Eventable
 
   acts_as_paranoid
 
-  has_one :resource, as: :resourceable
   has_many :track, as: :trackable
   belongs_to :project
+
+  # 任务状态机
+  aasm column: :status, no_direct_assignment: true do
+    state :status_fresh, initial: true # 新建
+    state :status_start                # 开始处理
+    state :status_pause                # 暂停
+    state :status_completed            # 完成
+    state :status_reopen               # 重新打开
+
+    # 开始处理
+    event :do_start do
+      transitions from: [:status_fresh, :status_pause, :status_reopen], to: :status_start
+    end
+
+    # 暂停
+    event :do_pause do
+      transitions from: :status_start, to: :status_pause
+    end
+
+    # 完成
+    event :do_complete do
+      transitions from: [:status_fresh, :status_start, :status_pause, :status_reopen], to: :status_completed
+    end
+
+    # 重新打开
+    event :do_reopen do
+      transitions from: :status_completed, to: :status_reopen
+    end
+  end
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -12,12 +12,12 @@ class TodoTest < ActiveSupport::TestCase
 
   test 'should create an event after create a new todo' do
     assert_difference('Event.count') do
-      Todo.create(project_id: @project.id, name: 'create_todo', creator_id: @current_user.id,  status: 'status_fresh')
+      Todo.create(project_id: @project.id, name: 'create_todo', creator_id: @current_user.id)
     end
   end
 
   test 'the ancestor and resource of todo event both should be the project' do
-    todo  = Todo.create(project_id: @project.id, name: 'new_todo', creator_id: @current_user.id,  status: 'status_fresh')
+    todo  = Todo.create(project_id: @project.id, name: 'new_todo', creator_id: @current_user.id)
     event = Event.last
 
     assert_equal event.team_id, @current_team.id
@@ -33,7 +33,7 @@ class TodoTest < ActiveSupport::TestCase
   end
 
   test 'the priority and tag of todo event both should be in the data' do
-    todo  = Todo.create(project_id: @project.id, name: 'new_todo', creator_id: @current_user.id,  status: 'status_fresh', priority: '!!!', tag: 'test')
+    todo  = Todo.create(project_id: @project.id, name: 'new_todo', creator_id: @current_user.id, priority: '!!!', tag: 'test')
     event = Event.last
 
     assert_equal event.team_id, @current_team.id
@@ -98,10 +98,11 @@ class TodoTest < ActiveSupport::TestCase
     assert_equal event.data["due"], { "prev" => nil, "after" => due.to_s }
   end
 
-  test 'should create an event after change todo status' do
-    @todo.update(status: "status_start")
-    event = Event.last
+  test 'should create an event after starting todo' do
+    @todo.do_start!
+    assert @todo.status_start?
 
+    event = Event.last
     assert_equal event.team_id, @current_team.id
     assert_equal event.resource_id, @project.resource.id
     assert_equal event.actor_id, @current_user.id
@@ -113,6 +114,63 @@ class TodoTest < ActiveSupport::TestCase
     assert_equal event.ancestor_type, @project.class.name
     assert_equal event.ancestor_name, @project.name
     assert_equal event.data['status'], { "prev" => 'status_fresh', 'after' => 'status_start' }
+  end
+
+  test 'should create an event after pausing todo' do
+    todo  = todos(:status_start_todo)
+    todo.do_pause!
+    assert todo.status_pause?
+
+    event = Event.last
+    assert_equal event.team_id, @current_team.id
+    assert_equal event.resource_id, @project.resource.id
+    assert_equal event.actor_id, @current_user.id
+    assert_equal event.action, "todo_status_pause"
+    assert_equal event.trackable_id, todo.id
+    assert_equal event.trackable_type, todo.class.name
+    assert_equal event.trackable_name, todo.name
+    assert_equal event.ancestor_id, @project.id
+    assert_equal event.ancestor_type, @project.class.name
+    assert_equal event.ancestor_name, @project.name
+    assert_equal event.data['status'], { "prev" => 'status_start', 'after' => 'status_pause' }
+  end
+
+  test 'should create an event after completing todo' do
+    todo  = todos(:status_pause_todo)
+    todo.do_complete!
+    assert todo.status_completed?
+
+    event = Event.last
+    assert_equal event.team_id, @current_team.id
+    assert_equal event.resource_id, @project.resource.id
+    assert_equal event.actor_id, @current_user.id
+    assert_equal event.action, "todo_status_completed"
+    assert_equal event.trackable_id, todo.id
+    assert_equal event.trackable_type, todo.class.name
+    assert_equal event.trackable_name, todo.name
+    assert_equal event.ancestor_id, @project.id
+    assert_equal event.ancestor_type, @project.class.name
+    assert_equal event.ancestor_name, @project.name
+    assert_equal event.data['status'], { "prev" => 'status_pause', 'after' => 'status_completed' }
+  end
+
+  test 'should create an event after reopening todo' do
+    todo  = todos(:status_completed_todo)
+    todo.do_reopen!
+    assert todo.status_reopen?
+
+    event = Event.last
+    assert_equal event.team_id, @current_team.id
+    assert_equal event.resource_id, @project.resource.id
+    assert_equal event.actor_id, @current_user.id
+    assert_equal event.action, "todo_status_reopen"
+    assert_equal event.trackable_id, todo.id
+    assert_equal event.trackable_type, todo.class.name
+    assert_equal event.trackable_name, todo.name
+    assert_equal event.ancestor_id, @project.id
+    assert_equal event.ancestor_type, @project.class.name
+    assert_equal event.ancestor_name, @project.name
+    assert_equal event.data['status'], { "prev" => 'status_completed', 'after' => 'status_reopen' }
   end
 
   test 'should create an event after destroy todo' do
